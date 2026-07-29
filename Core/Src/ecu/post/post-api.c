c962d1df6f413a4d0a48b7505fcbb51fedc04bec
Bridiro <ale.bridi15@gmail.com>
2026-07-29 21:03:56 +0200

    fix: add missing shutdown init in post

--- Core/Src/ecu/post/post-api.c
+++ Core/Src/ecu/post/post-api.c
@@ -13,6 +13,7 @@
 #include "pedals-api.h"
 #include "raspberry-api.h"
 #include "tractive-system-api.h"
+#include "shutdown-api.h"
 
 enum PostReturnCode post_api_do_init(struct PostConfig *post_config) {
     if (post_config == NULL) {
@@ -24,7 +25,8 @@ enum PostReturnCode post_api_do_init(struct PostConfig *post_config) {
         post_config->inverters_send_drive_command == NULL ||
         post_config->inverters_set_torque == NULL ||
         post_config->raspberry_pin_control == NULL ||
-        post_config->ts_send_command == NULL) {
+        post_config->ts_send_command == NULL ||
+        post_config->shutdown_set_state == NULL) {
         return POST_RC_ERROR;
     }
 
@@ -74,6 +76,10 @@ enum PostReturnCode post_api_do_init(struct PostConfig *post_config) {
     }
 
     if (ts_api_init(post_config->ts_send_command) != TS_RC_OK) {
+        final_status = POST_RC_ERROR;
+    }
+
+    if (shutdown_api_init(post_config->shutdown_set_state) != SHUTDOWN_RC_OK) {
         final_status = POST_RC_ERROR;
     }
 
